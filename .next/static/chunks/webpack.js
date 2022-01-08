05cc59d356c7f650f05bcb8eee0ddd74a8114a0b
dicky ardiar <dickyardiar1@gmail.com>
2022-01-08 18:59:56 +0700

    creare login page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5f1515cdebe18fad"; }
+/******/ 		__webpack_require__.h = function() { return "3597271c60af5437"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/Login.js
+++ pages/Login.js
@@ -1,20 +1,29 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Form, Card } from 'react-bootstrap'
 import style from '../styles/login_style/baselogin.module.scss'
-
+import Image from 'next/image'
+import LoginImage from '../assets/image_4_login.png'
 
 const Login = () => {
   return (
     <div className={style.baselogin}>
-      <Container className="mt-5 pt-5 ml-4 pl-4">
+      <Container style={{
+        padding: "16rem"
+      }} className="text-center">
+        <h6 className="text-center">Sign in To Task.io</h6>
         <Row md={12}>
-          <Col md={6}>
-            Form login is here
-          </Col>
-
-          <Col md={6}>
-            <p>This is image</p>
-          </Col>
+          <Card style={{ width: "20rem" }}>
+            <Card.Body>
+              <Col md={8}>
+              <Form>
+                <Form.Group className="mb-3 text-center">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" placeholder="email is here" />
+                </Form.Group>
+              </Form>
+              </Col>
+            </Card.Body>
+          </Card>
         </Row>
       </Container>
     </div>
