687825daafff679f7d3c82620d6d7e408be62502
dicky ardiar <dickyardiar1@gmail.com>
2022-01-29 18:51:30 +0700

    fixing header

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5d81c1ec6b5d73b2"; }
+/******/ 		__webpack_require__.h = function() { return "e7b5ad00bd1a5048"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.59ecf5226bd314ca.hot-update.js
+++ .next/static/webpack/webpack.59ecf5226bd314ca.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "fbc6428dc69acf7f"; }
+/******/ 	__webpack_require__.h = function() { return "8d8ef8e5f7972e4e"; }
 /******/ }();
 /******/ 
 /******/ }
